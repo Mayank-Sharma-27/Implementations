189b735d71107ebf39b045cc8dfa66ef961d7618
Mayank-Sharma-27 <mayank.sharma2796@gmail.com>
2025-07-12 08:50:26 -0700

    Currency conversion optimized solution

--- python/questions/currency_conversion.py
+++ python/questions/currency_conversion.py
@@ -7,64 +7,88 @@ Part 3: Write a method that returns the minimum cost and involved shipping metho
 
 During the phone screen, I successfully solved the first three parts and ran test cases for each. Unfortunately, I ran out of time before I could get to the fourth part. I coded in C++, though I’d recommend using a language like Python to simplify input parsing. """
 
+from collections import defaultdict
 
 class Conversion:
     
-    def parse_input(self, conversion_string: str) -> dict:
-        currecy_tokens = conversion_string.split(",")
-        conversion_dict = {}
-        for currency_token in currecy_tokens:
-            source = currency_token.split(":")[0].strip()
-            target = currency_token.split(":")[1].strip()
-            shipment = currency_token.split(":")[2].strip()
-            cost = currency_token.split(":")[3].strip()
-            key = f"{source}:{target}"
-            conversion_dict[key] = (shipment, float(cost))
+    def create_conversion_graph(self, log: str) -> dict :
+        graph = defaultdict(lambda : defaultdict(list))
         
-        return conversion_dict
-    
-    def convert(self, source: str, target: str, conversion_dict: dict, amount: str) -> float:
-        key = f"{source}:{target}"
-        amount = float(amount)
-        ans = -1
-        if key not in conversion_dict:
-            return -1
-        value = conversion_dict[key][1]
-        ans = float(amount * value)
-        return ans    
+        log_tokens = log.split(",")
+        for log_token in log_tokens:
+            source, destination, carrier, amount = log_token.split(":")
+            graph[source][destination].append({
+                "carrier": carrier,
+                "amount": int(amount)
+            })
         
-    def convert_with_one_hop(self, source: str, target: str, conversion_dict: dict, amount: str)  -> tuple[float, list[str]]:
-        min_cost = 10000000
-        ans = []
-        for middle in self.get_all_currencies(conversion_dict): 
-            key1 = f"{source}:{middle}"
-            key2 = f"{middle}:{target}"
-            if key1 in conversion_dict and key2 in conversion_dict:
-                ship1, cost1 = conversion_dict[key1]
-                ship2, cost2 = conversion_dict[key2]
-                total_cost = (cost1+cost2)*amount
-                
-                if (min_cost > total_cost):
-                    min_cost = total_cost
-                    ans = (total_cost, [ship1, ship2])  
-            return ans 
+        return graph
     
-    def get_all_currencies(self, conversion_dict: dict):
-        ans = set()
-        for key in conversion_dict.keys():
-            src, trg = key.split(":")
-            ans.add(src)
-            ans.add(trg)   
-        return ans     
+    def convert(self, log: str ,source: str, destination: str, amount: int) -> int:
+        graph = self.create_conversion_graph(log)
+        
+        if source not in graph or destination not in graph[source]:
+            return []
+        
+        destination_map = graph[source]
+        
+        if destination not in destination_map:
+            return ValueError("No destination present")
+        
+        results = []
+        for route in destination_map[destination]:
+            results.append({
+                "from": source,
+                "to": destination,
+                "carrier": route["carrier"],
+                "rate": route["amount"],
+                "total_cost": amount * route["amount"]
+            })
+        return results
+        
+        ans = sorted(ans, key=lambda a:a["conversion_cost"])       
+        return ans
     
+    def convert_with_one_hop(self, log: str ,source: str, required_destination: str, amount: int) -> dict:
+        graph = self.create_conversion_graph(log)
+        results = []
+
+        # Direct
+        results.extend(self.convert(log, source, required_destination, amount))
+
+        # One-hop
+        for mid in graph[source]:
+            if required_destination in graph[mid]:
+                for route1 in graph[source][mid]:
+                    for route2 in graph[mid][required_destination]:
+                        total_cost = amount * (route1["amount"] + route2["amount"])
+                        results.append({
+                            "from": source,
+                            "via": mid,
+                            "to": required_destination,
+                            "carriers": [route1["carrier"], route2["carrier"]],
+                            "rates": [route1["amount"], route2["amount"]],
+                            "total_cost": total_cost
+                        })
+        return results
+    
+    def minimum_cost_coversion(self, log: str, source: str, target: str) -> dict:
+        all_routes = self.convert_with_one_hop(log, source, target, 1.0)
+        if not all_routes:
+            return {}
+        return min(all_routes, key=lambda x: x["total_cost"])                 
+
+           
+       
 if __name__ == "__main__":
      conversion = Conversion()
      
      conversion_string = "USD:CAD:DHL:2,USD:GBP:FEDX:4,CAD:INR:UPS:3,GBP:INR:DHL:1"
      
-     conversion_dict = conversion.parse_input(conversion_string)
-     print(conversion.convert("USD", "CAD", conversion_dict, 10 ))
-     print(conversion.convert_with_one_hop("USD","INR", conversion_dict, 10))
+     
+     print(conversion.convert(conversion_string,"USD", "CAD", 10))
+     print(conversion.convert_with_one_hop(conversion_string, "USD","INR", 10))
+     print(conversion.minimum_cost_coversion(conversion_string, "USD","INR"))
      
         
         
